4fbad1e56c3d61d04ea5430f19d063ebed832adc
unknown <you@example.com>
2021-04-14 19:35:28 +0530

    validation first commit by joga

--- app/src/main/java/com/example/r_edu_kt/LoginActivity.java
+++ app/src/main/java/com/example/r_edu_kt/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.example.r_edu_kt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText nameEt,passwordEt;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +31,72 @@ public class LoginActivity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
         setContentView(R.layout.activity_login);
+        nameEt=findViewById(R.id.editTextName);
+        passwordEt=findViewById(R.id.editTextPaswword);
+        loginButton=findViewById(R.id.cirLoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
     }
 
     public void onLoginClick(View view) {
         startActivity(new Intent(this,RegisterActivity.class));
         overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+    }
+    void login(){
+      final  String name,password;
+        password=passwordEt.getText().toString();
+        name=nameEt.getText().toString();
+
+        if(TextUtils.isEmpty(name)){
+            nameEt.setError("Enter your mail");
+            return;
+        }
+        else if(TextUtils.isEmpty(password)){
+            passwordEt.setError("Enter your password1");
+            return;
+        }
+
+         DatabaseReference reference=FirebaseDatabase.getInstance().getReference("users");
+
+        Query checkUser=reference.orderByChild("name").equalTo(name);
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+               if(snapshot.exists()){
+                   String passwordFromDB=snapshot.child(name).child("password").getValue(String.class);
+                   if(password.equals(passwordFromDB)){
+                       String nameFromDB=snapshot.child(name).child("name").getValue(String.class);
+                       String phoneNoFromDB=snapshot.child(name).child("phoneNo").getValue(String.class);
+                       String emailFromDB=snapshot.child(name).child("email").getValue(String.class);
+
+                       Intent intent=new Intent(LoginActivity.this,UserProfile.class);
+
+                       intent.putExtra("name",nameFromDB);
+                       intent.putExtra("password",passwordFromDB);
+                       intent.putExtra("phoneNo",phoneNoFromDB);
+                       intent.putExtra("email",emailFromDB);
+                       startActivity(intent);
+
+                   }
+                   else{
+                        passwordEt.setError("Wrong Password");
+                        passwordEt.requestFocus();
+                   }
+               }
+               else{
+                   nameEt.setError("No such use exists");
+                   nameEt.requestFocus();
+               }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/r_edu_kt/RegisterActivity.java
+++ app/src/main/java/com/example/r_edu_kt/RegisterActivity.java
@@ -1,15 +1,36 @@
 package com.example.r_edu_kt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    EditText nameEt,emailEt,phonenumEt,passwordEt;
+    Button registerButton;
+
+    FirebaseDatabase rootNode;
+
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +38,22 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         changeStatusBarColor();
+
+
+        nameEt=findViewById(R.id.editTextName);
+        emailEt=findViewById(R.id.editTextEmail);
+        phonenumEt=findViewById(R.id.editTextMobile);
+        passwordEt=findViewById(R.id.editTextPaswword);
+        registerButton=findViewById(R.id.cirRegisterButton);
+
+        ProgressDialog progressDialog;
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
     }
     public void changeStatusBarColor()
     {
@@ -31,5 +68,53 @@ public class RegisterActivity extends AppCompatActivity {
     {
         startActivity(new Intent(this,LoginActivity.class));
         overridePendingTransition(R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+    void register(){
+        rootNode =FirebaseDatabase.getInstance();
+        reference=rootNode.getReference("users");
+       final String name,email,password,phoneNo;
+        name=nameEt.getText().toString();
+        email=emailEt.getText().toString();
+        password=passwordEt.getText().toString();
+        phoneNo=phonenumEt.getText().toString();
+
+         if(TextUtils.isEmpty(name)){
+            passwordEt.setError("Enter your name");
+            return;
+        }
+        else if(TextUtils.isEmpty(email)){
+            emailEt.setError("Enter your mail");
+            return;
+        }
+        else if(TextUtils.isEmpty(phoneNo)){
+             passwordEt.setError("Enter your password1");
+             return;
+         }
+        else if(TextUtils.isEmpty(password)){
+            passwordEt.setError("Enter your password1");
+            return;
+        }
+
+
+        Query checkUser=reference.orderByChild("name").equalTo(name);
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    Toast.makeText(RegisterActivity.this,"user already exists",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    userHelperClass helperClass=new userHelperClass(name,email,phoneNo,password);
+                    reference.child(name).setValue(helperClass);
+                    Toast.makeText(RegisterActivity.this,"Register Successfull",Toast.LENGTH_LONG).show();
+                    Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
